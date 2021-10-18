8cdf8c3a1bb5ddca42f205ccc9589ade8c8070de
akshay.babar <akshay.babar@biomarking.com>
2021-10-18 21:16:13 +0530

    create a route and modal schema

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import {userRoute} from "./routes/users.js";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false,useCreateIndex:true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+
+app.use(cors({origin: 'http://localhost:3000'}));
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+//middleware
+app.use(express.json());
+
+
+
+
+app.use("/api/users", userRoute);
+
+app.listen(8001,()=>{
+    console.log("server is started ");
+})
